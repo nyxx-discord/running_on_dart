9b70d62362ea81a966a7271c53fa42f713b3cd14
Szymon Uglis <szymon.uglis@tuta.io>
2020-06-08 20:36:08 +0200

    Use base url

--- bin/docs.dart
+++ bin/docs.dart
@@ -4,7 +4,7 @@ import "package:http/http.dart" as http;
 const baseUrl = "https://pub.dev/documentation/nyxx/latest/nyxx/";
 
 Future<String> getUrlToProperty(String className, String? fieldName) async {
-  final url = "https://pub.dev/documentation/nyxx/latest/nyxx/$className-class.html";
+  final url = "$baseUrl$className-class.html";
 
   if (fieldName == null) {
     return url;
@@ -19,7 +19,7 @@ Future<String> getUrlToProperty(String className, String? fieldName) async {
 }
 
 Future<Map<String, String>> searchDocs(String query) async {
-  final httpContent = await http.read("https://pub.dev/documentation/nyxx/latest/nyxx/nyxx-library.html");
+  final httpContent = await http.read("${baseUrl}nyxx-library.html");
   final document = html_parser.parse(httpContent);
   final elements = document.querySelectorAll("span.name > a").where((element) => element.innerHtml.toLowerCase().contains(query.toLowerCase())).take(8);
 
