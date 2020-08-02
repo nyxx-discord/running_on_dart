8c426a644fefa8f58f3d412ee1693492d7cba11a
Szymon Uglis <szymon.uglis@tuta.io>
2020-08-02 22:38:49 +0200

    Improve formatting.

--- bin/hot_reload/hot_reload.dart
+++ bin/hot_reload/hot_reload.dart
@@ -1,28 +1,28 @@
+import "dart:developer" as dev;
 import "dart:io";
 
 import "package:jaguar_hotreload/jaguar_hotreload.dart";
-import 'package:nyxx.commander/commander.dart';
-import 'package:path/path.dart' as path;
-import 'dart:developer' as dev;
+import "package:nyxx.commander/commander.dart";
+import "package:path/path.dart" as path;
 
 import "reloaded.dart";
 
-late HotReloader reloader;
+late HotReloader _reloader;
 
 Future<void> initReloader() async {
-  var info = await dev.Service.getInfo();
+  final info = await dev.Service.getInfo();
   var uri = info.serverUri!;
-  uri = uri.replace(path: path.join(uri.path, 'ws'));
-  if (uri.scheme == 'https') {
-    uri = uri.replace(scheme: 'wss');
+  uri = uri.replace(path: path.join(uri.path, "ws"));
+  if (uri.scheme == "https") {
+    uri = uri.replace(scheme: "wss");
   } else {
-    uri = uri.replace(scheme: 'ws');
+    uri = uri.replace(scheme: "ws");
   }
 
-  print('Hot reloading enabled');
-  reloader = HotReloader(vmServiceUrl: uri.toString());
-  reloader.addPath("/mnt/data3/PROJECTS/running_on_dart/bin/hot_reload/");
-  await reloader.go();
+  print("Hot reloading enabled");
+  _reloader = HotReloader(vmServiceUrl: uri.toString());
+  _reloader.addPath("/mnt/data3/PROJECTS/running_on_dart/bin/hot_reload/");
+  await _reloader.go();
 }
 
 Future<dynamic> hotReloadCode(String code, CommandContext context) async {
@@ -44,7 +44,7 @@ Future<dynamic> hotReloadCode(String code, CommandContext context) async {
   """;
 
   await File("/mnt/data3/PROJECTS/running_on_dart/bin/hot_reload/reloaded.dart").writeAsString(fileCode, mode: FileMode.writeOnly);
-  await reloader.reload();
+  await _reloader.reload();
 
   return execute(context);
 }
