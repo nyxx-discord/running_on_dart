23b502025d7f2409f0f77972d2a9feca46647c7b
Szymon Uglis <szymon.uglis@tuta.io>
2020-08-11 21:36:15 +0200

    Add support for docs from local file system

--- bin/docs.dart
+++ bin/docs.dart
@@ -1,13 +1,21 @@
 //import 'dart:io';
 
-import "dart:io";
+import "dart:io" show File, Platform;
 
+import "package:html/dom.dart" show Document;
 import "package:html/parser.dart" as html_parser;
 import "package:http/http.dart" as http;
+
+bool get _localFileSystem => Platform.environment["ROD_NYXX_DOCS_PATH"] != null;
 
 String get baseUrl =>
     Platform.environment["ROD_NYXX_DOCS_PATH"]
         ?? "https://pub.dev/documentation/nyxx/latest/nyxx/";
+
+Future<Document> _readDocumentFromUrl(String url) async {
+  final fileContentsFuture = _localFileSystem ? File(url).readAsString() : http.read(url);
+  return html_parser.parse(await fileContentsFuture);
+}
 
 Future<String> getUrlToProperty(String className, String? fieldName) async {
   final url = "$baseUrl$className-class.html";
@@ -16,8 +24,8 @@ Future<String> getUrlToProperty(String className, String? fieldName) async {
     return url;
   }
 
-  final httpContent = await http.read(url);
-  final document = html_parser.parse(httpContent);
+  final document = await _readDocumentFromUrl(url);
+
   final features = document.querySelectorAll("span.name > a");
   final foundRelativeUrl = features.firstWhere((element) => element.innerHtml == fieldName).attributes["href"];
 
@@ -25,8 +33,9 @@ Future<String> getUrlToProperty(String className, String? fieldName) async {
 }
 
 Future<Map<String, String>> searchDocs(String query) async {
-  final httpContent = await http.read("${baseUrl}nyxx-library.html");
-  final document = html_parser.parse(httpContent);
+  final url = "${baseUrl}nyxx-library.html";
+
+  final document = await _readDocumentFromUrl(url);
   final elements = document.querySelectorAll("span.name > a").where((element) => element.innerHtml.toLowerCase().contains(query.toLowerCase())).take(8);
 
   return <String, String>{
