d77b2bbbb34b3e823648c50f9ccea8e34b0750d4
Szymon Uglis <szymon@l7ssha.xyz>
2020-08-27 20:39:02 +0200

    Update exec module

--- bin/exec.dart
+++ bin/exec.dart
@@ -1,27 +1,28 @@
 import "dart:convert" show utf8;
 import "dart:io" show File, FileMode, Process;
 
+String getFileContent(String code) =>
+    """
+      import "dart:convert";
+      import "dart:io";
+      import "dart:async";
+      import "dart:collection";
+      import "dart:isolate";
+      import "dart:math";
+      import "dart:typed_data";
+    
+      FutureOr<dynamic> exeCode() async {
+        $code
+      }
+      
+      main() {
+        print(await exeCode());
+      }
+    """;
+
 /// Executes code not connected to main process, therefore doesnt have access to its data
 Future<String> eval(String code) async {
-  if (!code.contains("return")) {
-    code = "return $code";
-  }
-
-  if (!code.endsWith(";")) {
-    code = "$code;";
-  }
-
-  final fileCode = """
-    FutureOr<dynamic> exeCode() {
-      $code
-    }
-    
-    main() {
-      print(await exeCode());
-    }
-  """;
-
-  await File("/tmp/dart-eval.dart").writeAsString(fileCode, mode: FileMode.writeOnly);
+  await File("/tmp/dart-eval.dart").writeAsString(getFileContent(code), mode: FileMode.writeOnly);
   final process = await Process.run("dart", ["/tmp/dart-eval.dart"], stderrEncoding: utf8);
 
   final stdout = process.stdout.toString();
