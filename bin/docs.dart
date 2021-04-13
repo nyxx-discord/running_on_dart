da2708dab81bc7297f0b6f5a8e76a622663ebdca
Szymon Uglis <szymon.uglis@hotmail.com>
2021-04-14 01:48:57 +0200

    Add cache for fetching last doc update info; Update dependencies

--- bin/docs.dart
+++ bin/docs.dart
@@ -1,6 +1,9 @@
 import "dart:convert" show Utf8Decoder, jsonDecode;
 import "dart:io" show File, HttpClient;
 
+late DateTime lastDocUpdate;
+DateTime lastDocUpdateTimer = DateTime(2005);
+
 List<dynamic> _indexJson = jsonDecode(File("docfiles/nyxxdocs.json").readAsStringSync()) as List<dynamic>;
 String get basePath => "https://nyxx.l7ssha.xyz/";
 Uri get docUpdatePath => Uri.parse("https://api.github.com/repos/l7ssha/nyxx/actions/runs?status=success&per_page=1&page=1");
@@ -54,14 +57,21 @@ Iterable<DocDefinition> searchDocs(String query) sync* {
 }
 
 Future<DateTime> fetchLastDocUpdate() async {
-  final request = await HttpClient()
-      .getUrl(docUpdatePath);
+  if (lastDocUpdateTimer.difference(DateTime.now()).inMinutes.abs() > 15) {
+    final request = await HttpClient()
+        .getUrl(docUpdatePath);
 
-  final response = await request.close();
-  final body = await response.transform(const Utf8Decoder()).join();
-  final jsonBody = jsonDecode(body);
+    final response = await request.close();
+    final body = await response.transform(const Utf8Decoder()).join();
+    final jsonBody = jsonDecode(body);
 
-  return DateTime.parse(
-    jsonBody["workflow_runs"][0]["updated_at"] as String
-  );
+    final result = DateTime.parse(
+        jsonBody["workflow_runs"][0]["updated_at"] as String
+    );
+
+    lastDocUpdateTimer = DateTime.now();
+    lastDocUpdate = result;
+  }
+
+  return lastDocUpdate;
 }
