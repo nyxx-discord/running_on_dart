bf3b57b9b2f03001c45e27244225ad2c1626c4ae
Szymon Uglis <szymon@l7ssha.xyz>
2021-09-23 17:51:00 +0200

    Improve join logs event handling

--- lib/src/modules/joinLogs.dart
+++ lib/src/modules/joinLogs.dart
@@ -2,7 +2,6 @@ import "package:nyxx/nyxx.dart" show EmbedBuilder, GuildMemberAddEvent, Member, 
 import "package:running_on_dart/src/internal/utils.dart" show enabledIntentFeatures;
 import "package:running_on_dart/src/modules/settings/settings.dart" show fetchFeatureSettings, memberJoinLogsSettingName;
 
-Map<Snowflake, Snowflake> channelCache = {};
 
 Future<void> joinLogJoinEvent(GuildMemberAddEvent event) async {
   if (!enabledIntentFeatures) {
@@ -14,18 +13,8 @@ Future<void> joinLogJoinEvent(GuildMemberAddEvent event) async {
     return;
   }
 
-  var channelToSendMessageTo = channelCache[joinLogFeature.guildId];
-  if (channelToSendMessageTo == null) {
-    if (joinLogFeature.additionalData == null) {
-      return;
-    }
-
-    channelToSendMessageTo = Snowflake(joinLogFeature.additionalData);
-    channelCache[joinLogFeature.guildId] = channelToSendMessageTo;
-  }
-
   await event.member.client.httpEndpoints.sendMessage(
-      channelToSendMessageTo,
+      Snowflake(joinLogFeature.additionalData!),
       _getBuilderForMember(event.member, event.user)
   );
 }
