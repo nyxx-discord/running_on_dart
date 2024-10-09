3b41455eabace201cf6b0e38c5c4e3c668006537
Szymon Uglis <szymon@l7ssha.xyz>
2024-10-10 01:56:30 +0200

    Deploy 4.1.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 4.1.2
+
+- Improve Injector implementation
+
 ## 4.1.1
 
 - Fix docs search command
--- lib/src/settings.dart
+++ lib/src/settings.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 import 'package:nyxx/nyxx.dart';
 
-String get version => '4.1.1';
+String get version => '4.1.2';
 
 /// Get a [String] from an environment variable, throwing an exception if it is not set.
 ///
