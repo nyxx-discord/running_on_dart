73216c69835b21419c0a9fbe7a3462f808ed312e
Szymon Uglis <szymon@l7ssha.xyz>
2024-10-09 16:48:59 +0200

    Fix ping command

--- lib/src/commands/ping.dart
+++ lib/src/commands/ping.dart
@@ -14,8 +14,8 @@ final ping = ChatCommand(
   'ping',
   'Checks if the bot is online',
   id('ping', (ChatContext context) async {
-    final gatewayLatency = context.client.gateway.latency;
-    final restLatency = context.client.httpHandler.latency;
+    final gatewayLatency = context.client.gateway.latency.inMilliseconds;
+    final restLatency = context.client.httpHandler.latency.inMilliseconds;
 
     final embed = EmbedBuilder(color: getRandomColor(), fields: [
       EmbedFieldBuilder(name: 'Gateway latency', value: '${gatewayLatency}ms', isInline: true),
