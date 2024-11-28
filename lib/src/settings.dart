2f025ef989821aa36dcf1f000b9c297e27f97cd5
Szymon Uglis <szymon@l7ssha.xyz>
2024-11-28 17:56:30 +0100

    Release 4.10.0-dev.9

--- lib/src/settings.dart
+++ lib/src/settings.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 import 'package:nyxx/nyxx.dart';
 
-String get version => '4.10.0-dev.8';
+String get version => '4.10.0-dev.9';
 
 /// Get a [String] from an environment variable, throwing an exception if it is not set.
 ///
